9380423acc3e00bdcc69051272b6e963a02835e3
00goldfish00 <dp53899@gmail.com>
2021-11-03 16:07:39 -0500

    Removed all uses of tkinter

--- control_server.py
+++ control_server.py
@@ -56,19 +56,18 @@ class MyHandler(http.server.BaseHTTPRequestHandler):
                 
                 # create HTML code with updated slider and status variable to be sent back to web page
                 body = ('<form action="/send_color" method="post">\n'
-                                f'<input type="range" name="r_range" min="0" max="100" value="{gui.r_val.get()}" />\n'
+                                f'<input type="range" name="r_range" min="0" max="100" value="{r_val}" />\n'
                                 '<label>Red</label><br>\n'
-                                f'<input type="range" name="g_range" min="0" max="100" value="{gui.g_val.get()}" />\n'
+                                f'<input type="range" name="g_range" min="0" max="100" value="{g_val}" />\n'
                                 '<label>Green</label><br>\n'
-                                f'<input type="range" name="b_range" min="0" max="100" value="{gui.b_val.get()}" />\n'
+                                f'<input type="range" name="b_range" min="0" max="100" value="{b_val}" />\n'
                                 '<label>Blue</label><br>\n'
                                 '<input type="submit" name="color" value="Send Color" />\n'
                             '</form>\n'
                             '<form action="/send_toggle" method="post">\n'
                                 '<input type="submit" name="toggle" value="TOGGLE LED" />\n'
                             '</form>\n'
-                            f'<p>LED is {"OFF" if gui.led_state.get() == 0 else "ON"}</p>\n'
-                            f'<p>Door is {"CLOSED" if gui.door_state.get() == 0 else "OPEN"}</p>\n'
+                            f'<p>LED is {"OFF" if led_state else "ON"}</p>\n'
                             '<form action="/" method="post">\n'
                                 '<input type="submit" value="Update Page" />\n'
                             '</form>\n'
@@ -86,6 +85,8 @@ class MyHandler(http.server.BaseHTTPRequestHandler):
                 
     
     def do_POST(self):
+
+        #global r_val, g_val, b_val, led_state
 
         # call empty do_GET to update web page variables if update page button was pressed
         if self.path == "/":
@@ -112,9 +113,9 @@ class MyHandler(http.server.BaseHTTPRequestHandler):
             })
             
             if form["color"].value == "Send Color":
-                gui.r_val.set(form["r_range"].value)
-                gui.g_val.set(form["g_range"].value)
-                gui.b_val.set(form["b_range"].value)
+                r_val = form["r_range"].value
+                g_val = form["g_range"].value
+                b_val = form["b_range"].value
             
             self.do_GET()
             return
@@ -131,7 +132,7 @@ class MyHandler(http.server.BaseHTTPRequestHandler):
             })
             
             if form["toggle"].value == "TOGGLE LED":
-               gui.toggle_led()
+               led_state = not led_state
             
             self.do_GET()
             return
@@ -152,14 +153,10 @@ def run_server(html_server):
 # method that updates the RGB LED output
 def run_gpio():
     while True:
-        if led_state == True:
-            r_pwm.ChangeDutyCycle(r_val)
-            g_pwm.ChangeDutyCycle(g_val)
-            b_pwm.ChangeDutyCycle(b_val)
+        if led_state:
+            data_pwm.ChangeDutyCycle(r_val)
         else:
-            r_pwm.ChangeDutyCycle(0)
-            g_pwm.ChangeDutyCycle(0)
-            b_pwm.ChangeDutyCycle(0)
+            data_pwm.ChangeDutyCycle(0)
 
 
 # method is called when GUI is closed to clean up the program
@@ -167,29 +164,18 @@ def exit_protocall():
     print('Running GPIO cleanup')
     IO.cleanup()
     print('Running socket cleanup')
-    my_server.socket.close()
+    server.socket.close()
 
 
 # main code body
 if __name__ == '__main__':
+    
     # setup port and localhost
     PORT = 8000
     HOST = '0.0.0.0'
-
-    # setup pin names
-    RPIN = 11
-    GPIN = 13
-    BPIN = 15
-    LEDBTN = 19
     
-    # setup board pins and interrupts
-    IO.setmode(IO.BOARD)
-    IO.setwarnings(False)
-
-    # init pins to output
-    IO.setup(RPIN, IO.OUT)
-    IO.setup(GPIN, IO.OUT)
-    IO.setup(BPIN, IO.OUT)
+    # setup pin names
+    DATA_PIN = 11
 
     # init color values to black and LED strip to off
     r_val = 0
@@ -197,22 +183,25 @@ if __name__ == '__main__':
     b_val = 0
     led_state = False
 
-    # init PWM objects to zero
-    r_pwm = IO.PWM(RPIN, 60) # (pin, Hz)
-    r_pwm.start(0)
-    g_pwm = IO.PWM(GPIN, 60)
-    g_pwm.start(0)
-    b_pwm = IO.PWM(BPIN, 60)
-    b_pwm.start(0)
+    # setup board pins and interrupts
+    IO.setmode(IO.BOARD)
+    IO.setwarnings(False)
 
+    # init pins to output
+    IO.setup(DATA_PIN, IO.OUT)
+
+    # init PWM objects to zero
+    data_pwm = IO.PWM(DATA_PIN, 60) # (pin, Hz)
+    data_pwm.start(0)
+    
     # create and start thread to update the LEDs
     gpio_thread = Thread(target=run_gpio)
     gpio_thread.daemon = True
     gpio_thread.start()
 
     # create and start thread to run the server
-    my_server = http.server.HTTPServer((HOST, PORT), MyHandler)
-    server_thread = Thread(target=run_server, args=(my_server,))
+    server = http.server.HTTPServer((HOST, PORT), MyHandler)
+    server_thread = Thread(target=run_server, args=(server,))
     server_thread.daemon = True
     server_thread.start()
     
